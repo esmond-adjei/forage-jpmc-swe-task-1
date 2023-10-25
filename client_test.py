776f5cc6f9bfababa11814aa8e79e5f117b227f1
esmond-adjei <xmongram@gmail.com>
2023-10-25 01:57:02 +0000

    modified some files

--- client_test.py
+++ client_test.py
@@ -1,24 +1,43 @@
 import unittest
 from client3 import getDataPoint
 
+
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+                'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+             'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        """ ------------ Add the assertion below ------------ """
+        stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+        self.assertEqual(price, 120.48)
+        self.assertEqual(stock, 'ABC')
+        self.assertEqual(bid_price, 120.48)
+        self.assertEqual(ask_price, 121.2)
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+        stock, bid_price, ask_price, price = getDataPoint(quotes[1])
+        self.assertEqual(price, 117.87)
+        self.assertEqual(stock, 'DEF')
+        self.assertEqual(bid_price, 117.87)
+        self.assertEqual(ask_price, 121.68)
 
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+                'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+             'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        """ ------------ Add the assertion below ------------ """
+        stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+        self.assertGreater(bid_price, ask_price)
 
-  """ ------------ Add more unit tests ------------ """
+        stock, bid_price, ask_price, price = getDataPoint(quotes[1])
+        self.assertLess(bid_price, ask_price)
 
+    """ ------------ Add more unit tests ------------ """
 
 
 if __name__ == '__main__':
